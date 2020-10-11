39b80183bdbe4305957768d55142ee5fe4b3eb42
Shubham Chaudhary <shubham.bspdesign@gmail.com>
2020-10-11 09:48:21 +0530

    Navigation bar added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from "react";
 import "./App.css";
+import Nav from "./Nav";
 import { endPoints } from "./request";
 import Row from "./Row";
 
 function App() {
   return (
     <div className="app">
-      <Row
-        path={endPoints.netflixOriginals}
-        title={"NETFLIX ORIGINALS"}
-        isLarge
-      />
-      <Row path={endPoints.trendingNow} title={"Trending Now"} />
-      <Row path={endPoints.topRated} title={"Top Rated"} />
-      <Row path={endPoints.actionMovies} title={"Action Movies"} />
+      <Nav />
+      <div className="rows">
+        <Row
+          path={endPoints.netflixOriginals}
+          title={"NETFLIX ORIGINALS"}
+          isLarge
+        />
+        <Row path={endPoints.trendingNow} title={"Trending Now"} />
+        <Row path={endPoints.topRated} title={"Top Rated"} />
+        <Row path={endPoints.actionMovies} title={"Action Movies"} />
+      </div>
     </div>
   );
 }
